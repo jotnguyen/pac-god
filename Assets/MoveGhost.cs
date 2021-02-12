a347216c160aa36d5904b719a761da3bb7f12aee
John Nguyen <johntnguyen9@gmail.com>
2021-02-12 14:37:49 -0600

    Update working waypoint movement and Pac-God dies on collision now

--- Assets/MoveGhost.cs
+++ Assets/MoveGhost.cs
@@ -5,29 +5,34 @@ using UnityEngine;
 public class MoveGhost : MonoBehaviour
 {
     public Transform[] waypoints;
-    int cur = 0;
+
+    public static int cur = 0;
 
     public float speed = 0.02f;
     void FixedUpdate()
     {
         // Move closer to waypoint until touching waypoint
-        
-        if (transform.position != waypoints[cur].position)
+        //transform.position != waypoints[cur].position
+        if (Vector2.Distance(transform.position, waypoints[cur].position) > 0.1f)
         {
-
+            Debug.Log(cur + "if");
             Vector2 p = Vector2.MoveTowards(transform.position,
                                             waypoints[cur].position,
                                             speed);
             GetComponent<Rigidbody2D>().MovePosition(p);
         }
         // Select next waypoint after reaching first waypoint
-        //else cur = (cur + 1) % waypoints.Length;
-        else if (cur == waypoints.Length) { cur = 0; } else { cur++; }
+        else
+        {
+            //cur = cur + 1;
+            cur = (cur + 1) % waypoints.Length;
+            Debug.Log(cur);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.name == "pacman")
+        if (collider.gameObject.tag == "Player")
             Destroy(collider.gameObject);
     }
 }
