ce69fca71093b0e946f79e8c92b1b72e6c29fe51
John Nguyen <johntnguyen9@gmail.com>
2021-02-12 11:58:31 -0600

    Add red ghost and waypoints for movement

---
+++ Assets/MoveGhost.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGhost : MonoBehaviour
+{
+    public Transform[] waypoints;
+    int cur = 0;
+
+    public float speed = 0.02f;
+    void FixedUpdate()
+    {
+        // Move closer to waypoint until touching waypoint
+        
+        if (transform.position != waypoints[cur].position)
+        {
+
+            Vector2 p = Vector2.MoveTowards(transform.position,
+                                            waypoints[cur].position,
+                                            speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        // Select next waypoint after reaching first waypoint
+        //else cur = (cur + 1) % waypoints.Length;
+        else if (cur == waypoints.Length) { cur = 0; } else { cur++; }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "pacman")
+            Destroy(collider.gameObject);
+    }
+}
